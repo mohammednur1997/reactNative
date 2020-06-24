a1a6b09e4b6bba2b3c71bcee091ce77f25724b9d
mohammednur1997 <mm887549@gmail.com>
2020-06-24 19:28:45 +0300

    first push

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import View from "react-native"
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+
+            </View>
+    );
+    }
+}
+
+export default App;
